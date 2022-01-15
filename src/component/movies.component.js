929be76778cfed2c91190fadf7010cecbbdc6bfb
Abid Hasan Mahmud <41523592+abidmahmud@users.noreply.github.com>
2022-01-15 15:59:47 +0600

    API Push

--- src/component/movies.component.js
+++ src/component/movies.component.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 import Rating from './rating.component';
 import Table from './common/table.component';
+import getMovies from '../service/get-movies.service';
+import getGenres from '../service/get-genres.service';
 
 class Movies extends React.Component {
     state = {
-        movies: [
-            { rank: 1, movie_name: 'The Shawshank Redemption', imdb_rating: '9.2', release_date: '1994', your_rating: true },
-            { rank: 2, movie_name: 'The Godfather', imdb_rating: '9.1', release_date: '1972', your_rating: false },
-            { rank: 3, movie_name: 'The Shawshank: PartII', imdb_rating: '9.0', release_date: '1974', your_rating: false },
-            { rank: 4, movie_name: 'The Dark Knight', imdb_rating: '9.0', release_date: '2008', your_rating: true },
-            { rank: 5, movie_name: '12 Angry Man', imdb_rating: '8.9', release_date: '1957', your_rating: false },
-        ]
+        movies: [],
+        genres: []
+    }
+
+    componentDidMount() {
+        const movies = getMovies();
+        const genres = getGenres();
+        this.setState({ movies, genres });
     }
 
     handleToggleRating = movieRank => {
         const movies = [...this.state.movies];
-        const movie = movies.find(movie => movie.rank === movieRank);
+        const movie = (movie => movie.id === movieRank);
         movie.your_rating = !(movie.your_rating);
         this.setState({ movies });
     }
 
     render() {
         const columns = [
-            { label: 'Rank', path: 'rank', content: (movie, key) => <td>{movie[key]}</td> },
-            { label: 'Movie Name', path: 'movie_name', content: (movie, key) => <td>{movie[key]}</td> },
-            { label: 'Imdb_Rating', path: 'imdb_rating', content: (movie, key) => <td>{movie[key]}</td> },
-            { label: 'Release Date', path: 'release_date', content: (movie, key) => <td>{movie[key]}</td> },
-            { label: 'Your Rating', path: 'your_rating', content: (movie, key) => <td><Rating isRated={movie[key]} rank={movie.rank} handleToggleRating={this.handleToggleRating} /></td> },
+            { label: 'Rank', path: 'id', content: (movie, key) => <td>{movie[key]}</td> },
+            { label: 'Movie Name', path: 'title', content: (movie, key) => <td>{movie[key]}</td> },
+            { label: 'Genre', path: 'genre', content: (movie, key) => <td>{movie[key]}</td> },
+            { label: 'Poster', path: 'posterUrl', content: (movie, key) => <td><img src={movie[key]} style={{ height: "100px", width: "auto" }} /></td> },
+            { label: 'Release Date', path: 'year', content: (movie, key) => <td>{movie[key]}</td> },
+            { label: 'Your Rating', path: 'your_rating', content: (movie, key) => <td><Rating isRated={movie[key]} rank={movie.id} handleToggleRating={this.handleToggleRating} /></td> },
         ]
 
         return (
